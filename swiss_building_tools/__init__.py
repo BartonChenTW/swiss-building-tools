9715f1ff4ce9396a603b0e1c33ca4fb2646a84a0
chyi <yi-chung.chen@empa.ch>
2025-11-06 17:06:29 +0100

    added get_df_by_key in the init setup

--- swiss_building_tools/__init__.py
+++ swiss_building_tools/__init__.py
@@ -7,12 +7,13 @@ from .mapping import (
     get_egid_from_address,
     get_egid_house_no_from_request
 )
-from .utils import li2str, expand_house_number_ranges
+from .utils import li2str, expand_house_number_ranges, get_df_by_key
 
 __all__ = [
     'get_bldg_attrs_from_address',
     'get_egid_from_address',
     'get_egid_house_no_from_request',
     'li2str',
-    'expand_house_number_ranges'
+    'expand_house_number_ranges',
+    'get_df_by_key'
 ]
